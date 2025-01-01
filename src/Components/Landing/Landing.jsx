0a92b81bf5635e6c7a57a21b60248965259a9951
Raafky <raafkieducation@gmail.com>
2025-01-01 14:52:10 +0700

    Deskripsi perubahan

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import "./Landing.css"
+import React from "react";
+import "./Landing.css";
 
 function Landing() {
   return (
-    <div className='containerLanding'>
-        <h1 className='judulLanding'>Unteyo Journey</h1>
-        <h2 className='judulLanding2'>Greeting Quality Students</h2>
-        <h4 className='tagLanding'>#BeWise</h4>
+    <div className="containerLanding">
+      <h1 className="judulLanding">Farhan KUN</h1>
+      <h2 className="judulLanding2">Greeting Quality Students</h2>
+      <h4 className="tagLanding">#BeWise</h4>
     </div>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
