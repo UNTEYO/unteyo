4ea59d1832651eb72ccad43b3c85ac8432d9c9ca
Raafki <raafkieducation@gmail.com>
2025-01-09 08:24:02 +0700

    hal4

--- src/Components/Halaman4/Halaman4.jsx
+++ src/Components/Halaman4/Halaman4.jsx
@@ -7,34 +7,36 @@ import "./Halaman4.css";
 export default function Halaman4() {
   return (
     <>
-      <div className="containerHalaman4">
-        <section className="isiHalaman4">
-          <h2 className="headingHalaman4">Let's Connect</h2>
-          <h2 className="headingHalaman4Small">With Us!</h2>
-        </section>
-        <div className="photo-container">
-          {/* <div className="abstract-container">
+      <div className="page-4">
+        <div className="containerHalaman4">
+          <section className="isiHalaman4">
+            <h2 className="headingHalaman4">Let's Connect</h2>
+            <h2 className="headingHalaman4Small">With Us!</h2>
+          </section>
+          <div className="photo-container">
+            <div className="abstract-container">
             <div className="blob blob-1"></div>
             <div className="blob blob-2"></div>
             <div className="blob blob-3"></div>
-          </div> */}
-        </div>
-        <div class="triangle-gradient"></div>
-        <div class="right-triangle-gradient"></div>
-        <div className="dukunganNpartner">
-          <section>
-            <h4>Supported By :</h4>
-            <div className="wadahLogoDukungan">
-              <img className="logoDukungan" src={hubung} alt="" />
-            </div>
-          </section>
-          <section>
-            <h4>Partner Us :</h4>
-            <div className="wadahLogoDukungan">
-              <img className="logoDukungan" src={terai} alt="" />
-              <img className="logoDukungan" src={perspektif} alt="" />
-            </div>
-          </section>
+          </div>
+          </div>
+          <div class="triangle-gradient"></div>
+          <div class="right-triangle-gradient"></div>
+          <div className="dukunganNpartner">
+            <section>
+              <h4>Supported By :</h4>
+              <div className="wadahLogoDukungan">
+                <img className="logoDukungan" src={hubung} alt="" />
+              </div>
+            </section>
+            <section>
+              <h4>Partner Us :</h4>
+              <div className="wadahLogoDukungan">
+                <img className="logoDukungan" src={terai} alt="" />
+                <img className="logoDukungan" src={perspektif} alt="" />
+              </div>
+            </section>
+          </div>
         </div>
       </div>
     </>
