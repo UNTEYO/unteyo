5234f3ccf9e446871a09d72a0abc82ec1dbccc30
farhandwk <farhandwiki15@gmail.com>
2025-01-23 16:31:29 +0000

    mobile navbar landing, halaman 1, halaman 2

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -13,6 +13,28 @@ function Header() {
     console.log(`Tampilan mobile: ${tampilanMobile}`);
   }, [windowWidth, tampilanMobile]);
 
+  function onNav () {
+    const HamburgerOpen = document.querySelector(".hamburgerHeader")
+    const navbar = document.querySelector(".navbarHeader")
+    const navClose = document.querySelector(".navClose")
+    
+    navbar.style.transform = "translateY(0)"
+    navClose.style.transform = "translateY(0)"
+    navbar.style.transition = "0.5s"
+    navClose.style.transition = "0.5s"
+  }
+
+  function outNav () {
+    const HamburgerOpen = document.querySelector(".hamburgerHeader")
+    const navbar = document.querySelector(".navbarHeader")
+    const navClose = document.querySelector(".navClose")
+    
+    navbar.style.transform = "translateY(-35vh)"
+    navClose.style.transform = "translateY(-45vh)"
+    navbar.style.transition = "0.5s"
+    navClose.style.transition = "0.5s"
+  }
+
   return (
     <div
       className={`containerHeader ${
@@ -24,6 +46,7 @@ function Header() {
         className={`hamburgerHeader ${
           tampilanMobile ? "mobile-hamburgerHeader" : "hamburgerHeader"
         }`}
+        onClick={onNav}
       />
       <img
         src={Logo}
@@ -41,6 +64,19 @@ function Header() {
         <li>News</li>
         <li>Contact Us</li>
       </ul>
+      <div className={`navClose ${
+          tampilanMobile ? "mobile-navClose" : "navClose"
+        }`}>
+          <FontAwesomeIcon icon={faBars}
+          className={`hamburgerClose ${
+            tampilanMobile ? "mobile-hamburgerClose" : "hamburgerClose"
+          }`}
+          onClick={outNav}
+          ></FontAwesomeIcon>
+          <img src={Logo} className={`logoCloase ${
+            tampilanMobile ? "mobile-logoClose" : "logoClose"
+          }`}></img>
+        </div>
     </div>
   );
 }
--- src/Components/Landing/Decor.jsx
+++ src/Components/Landing/Decor.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { handleResponsive } from "../../handleResponsive";
 import Like from "../../assets/like.png";
 import Love from "../../assets/love.png";
 import Spiral from "../../assets/spiral.png";
@@ -8,17 +9,38 @@ import Spray3 from "../../assets/spray 3.png";
 import "./Decor.css";
 
 function Decor() {
+   const { windowWidth, tampilanMobile } = handleResponsive();
+  
+    useEffect(() => {
+      console.log(`Lebar sekarang: ${windowWidth}`);
+      console.log(`Tampilan mobile: ${tampilanMobile}`);
+    }, [windowWidth, tampilanMobile]);
+
   return (
     <div>
-      <img src={Like} className="likeDecor"></img>
-      <img src={Love} className="loveDecor"></img>
+      <img src={Like} className={`likeDecor ${
+        tampilanMobile ? "mobile-likeDecor" : "likeDecor"
+      }`}></img>
+      <img src={Love} className={`loveDecor ${
+        tampilanMobile ? "mobile-loveDecor" : "loveDecor"
+      }`}></img>
       {/* <img src={Spiral} className='spiralDecor'></img> */}
-      <div className="sprayDecor1"></div>
-      <div className="sprayDecor1-1"></div>
-      <div className="sprayDecor2"></div>
-      <div className="sprayDecor2-2"></div>
+      <div className={`sprayDecor1 ${
+        tampilanMobile ? "mobile-sprayDecor1" : "sprayDecor1"
+      }`}></div>
+      <div className={`sprayDecor1-1 ${
+        tampilanMobile ? "mobile-sprayDecor1-1" : "sprayDecor1-1"
+      }`}></div>
+      <div className={`sprayDecor2 ${
+        tampilanMobile ? "mobile-sprayDecor2" : "sprayDecor2"
+      }`}></div>
+      <div className={`sprayDecor2-2 ${
+        tampilanMobile ? "mobile-sprayDecor2-2" : "sprayDecor2-2"
+      }`}></div>
       <div className="sprayDecor2-2-2"></div>
-      <div className="sprayDecor3"></div>
+      <div className={`sprayDecor3 ${
+        tampilanMobile ? "mobile-sprayDecor3" : "sprayDecor3"
+      }`}></div>
       {/* <img src={Spray1} className='spray1Decor'></img>
       <img src={Spray2} className='spray2Decor'></img>
       <img src={Spray3} className='spray3Decor'></img> */}
