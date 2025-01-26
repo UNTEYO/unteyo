699f14dcef0b4af124efaba79a4a3d0309b861db
farhandwk <farhandwiki15@gmail.com>
2025-01-26 15:51:38 +0700

    backdrop header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,30 +9,46 @@ function Header() {
   const { windowWidth, tampilanMobile } = handleResponsive();
 
   useEffect(() => {
+    const handleScroll = () => {
+      const header = document.querySelector(".containerHeader");
+      if (window.scrollY > 0) {
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
     console.log(`Lebar sekarang: ${windowWidth}`);
     console.log(`Tampilan mobile: ${tampilanMobile}`);
   }, [windowWidth, tampilanMobile]);
 
-  function onNav () {
-    const HamburgerOpen = document.querySelector(".hamburgerHeader")
-    const navbar = document.querySelector(".navbarHeader")
-    const navClose = document.querySelector(".navClose")
-    
-    navbar.style.transform = "translateY(0)"
-    navClose.style.transform = "translateY(0)"
-    navbar.style.transition = "0.5s"
-    navClose.style.transition = "0.5s"
+  function onNav() {
+    const navbar = document.querySelector(".navbarHeader");
+    const navClose = document.querySelector(".navClose");
+
+    navbar.style.transform = "translateY(0)";
+    navClose.style.transform = "translateY(0)";
+    navbar.style.transition = "0.5s";
+    navClose.style.transition = "0.5s";
   }
 
-  function outNav () {
-    const HamburgerOpen = document.querySelector(".hamburgerHeader")
-    const navbar = document.querySelector(".navbarHeader")
-    const navClose = document.querySelector(".navClose")
-    
-    navbar.style.transform = "translateY(-35vh)"
-    navClose.style.transform = "translateY(-45vh)"
-    navbar.style.transition = "0.5s"
-    navClose.style.transition = "0.5s"
+  function outNav() {
+    const navbar = document.querySelector(".navbarHeader");
+    const navClose = document.querySelector(".navClose");
+
+    navbar.style.transform = "translateY(-35vh)";
+    navClose.style.transform = "translateY(-49vh)";
+    navbar.style.transition = "0.5s";
+    navClose.style.transition = "0.5s";
   }
 
   return (
@@ -67,16 +83,16 @@ function Header() {
       <div className={`navClose ${
           tampilanMobile ? "mobile-navClose" : "navClose"
         }`}>
-          <FontAwesomeIcon icon={faBars}
-          className={`hamburgerClose ${
-            tampilanMobile ? "mobile-hamburgerClose" : "hamburgerClose"
-          }`}
-          onClick={outNav}
-          ></FontAwesomeIcon>
-          <img src={Logo} className={`logoCloase ${
-            tampilanMobile ? "mobile-logoClose" : "logoClose"
-          }`}></img>
-        </div>
+        <FontAwesomeIcon icon={faBars}
+        className={`hamburgerClose ${
+          tampilanMobile ? "mobile-hamburgerClose" : "hamburgerClose"
+        }`}
+        onClick={outNav}
+        ></FontAwesomeIcon>
+        <img src={Logo} className={`logoCloase ${
+          tampilanMobile ? "mobile-logoClose" : "logoClose"
+        }`}></img>
+      </div>
     </div>
   );
 }
