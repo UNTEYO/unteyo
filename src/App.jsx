2c8018e4c822df6c5c00abf6b46d1c7daaf26777
Raafki <raafkieducation@gmail.com>
2025-01-27 11:18:24 +0700

    update Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
-import React from "react";
 import "./App.css";
-import Landing from "./Components/Landing/Landing";
-import Decor from "./Components/Landing/Decor";
-import Header from "./Components/Header/Header";
-import Footer from "./Components/Footer/Footer";
-import Halaman1 from "./Components/Halaman1/Halaman1";
-import Halaman2 from "./Components/Halaman 2/Halaman2";
-import Halaman3 from "./Components/Halaman 3/Halaman3"
-import Halaman4 from "./Components/Halaman4/Halaman4";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLanding from "./pages/MainLanding";
 
 function App() {
   return (
-    <div className="containerAll">
-      <Header />
-      <Landing />
-      <Decor />
-      <Halaman1 />
-      <Halaman2></Halaman2>
-      <Halaman3></Halaman3>
-      <Halaman4></Halaman4>
-      <Footer></Footer>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/unteyo" element={<MainLanding />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/MainLanding.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../App.css";
+import Decor from "../Components/Landing/Decor";
+import Landing from "../Components/Landing/Landing";
+import Header from "../Components/Header/Header";
+import Halaman1 from "../Components/Halaman1/Halaman1";
+import Halaman2 from "../Components/Halaman 2/Halaman2";
+import Halaman3 from "../Components/Halaman 3/Halaman3";
+import Halaman4 from "../Components/Halaman4/Halaman4";
+import Footer from "../Components/Footer/Footer";
+
+function MainLanding() {
+  return (
+    <div className="containerAll">
+      <Header></Header>
+      <Landing></Landing>
+      <Decor></Decor>
+      <Halaman1></Halaman1>
+      <Halaman2></Halaman2>
+      <Halaman3></Halaman3>
+      <Halaman4></Halaman4>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default MainLanding;
