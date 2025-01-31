970eee34ae3f97dd73443a1a65325941febf8e71
Raafki <raafkieducation@gmail.com>
2025-01-31 14:38:13 +0700

    Router update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import "./App.css";
 import CookieBanner from "./Components/Footer/CookieBanner";
 import CookiePolicy from "./Components/Footer/CookiePolicy";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import MainLanding from "./pages/MainLanding";
 import NotFound from "./pages/NotFound";
 import PrivacyPolicy from "./Components/Footer/PrivacyPolicy";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
-        <Route path="/unteyo" element={<MainLanding />} />
-        <Route path="/unteyo/privacy&policy" element={<PrivacyPolicy />} />
-        <Route path="/unteyo/cookiepolicy" element={<CookiePolicy />} />
+        <Route path="/" element={<MainLanding />} />
+        <Route path="/privacy&policy" element={<PrivacyPolicy />} />
+        <Route path="/cookiepolicy" element={<CookiePolicy />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <CookieBanner />
-    </Router>
+    </HashRouter>
   );
 }
 
