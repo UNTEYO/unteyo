892226adb3fe31e974ed4b2e02bf566de03e5ff5
farhandwk <farhandwiki15@gmail.com>
2025-02-05 01:30:00 +0700

    logika news jadi, kurang style

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -2,28 +2,110 @@
 import React, { useEffect, useState } from 'react';
 import { handleResponsive } from "../../handleResponsive";
 import { generateSequentialNumber } from './NewsCount'
+import News1 from "../../assets/News/News1.jpg"
+import News2 from "../../assets/News/News2.jpg"
+import News3 from "../../assets/News/News3.jpg"
 import "./News.css";
 
 function News() {
     const { windowWidth, tampilanMobile } = handleResponsive();
-    const [number, setNumber] = useState(0);
+    let [number, setNumber] = useState(0);
+    let [isHovered, setIsHovered] = useState(false)
+
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    let interval = setInterval(() => {
       setNumber(generateSequentialNumber());
-    }, 1000);
+    }, 2000);
 
     return () => clearInterval(interval);
   }, []);
 
       useEffect(() => {
-        
       }, [windowWidth, tampilanMobile]);
+
+    useEffect(() => {
+    const news1 = document.querySelector(".news1");
+    const news2 = document.querySelector(".news2");
+    const news3 = document.querySelector(".news3");
+    const img = document.querySelector(".imgNews");
+
+    const handleHover1 = () => {
+        setIsHovered(true)
+        news1.classList.add("newsHovered")
+    }
+
+    const outhover1 = () => {
+        setIsHovered(false)
+        news1.classList.remove("newsHovered")
+    }
+    const handleHover2 = () => {
+        setIsHovered(true)
+        news2.classList.add("newsHovered")
+    }
+
+    const outhover2 = () => {
+        setIsHovered(false)
+        news2.classList.remove("newsHovered")
+    }
+    const handleHover3 = () => {
+        setIsHovered(true)
+        news3.classList.add("newsHovered")
+    }
+
+    const outhover3 = () => {
+        setIsHovered(false)
+        news3.classList.remove("newsHovered")
+    }
+
+    news1.addEventListener("mouseover", handleHover1)
+    news1.addEventListener("mouseout", outhover1)
+
+    news2.addEventListener("mouseover", handleHover2)
+    news2.addEventListener("mouseout", outhover2)
+
+    news3.addEventListener("mouseover", handleHover3)
+    news3.addEventListener("mouseout", outhover3)
+
+    if (isHovered == true) {
+        if (news1.classList.contains("newsHovered")) {
+            img.src = News1
+        }
+        if (news2.classList.contains("newsHovered")) {
+            img.src = News2
+        }
+        if (news3.classList.contains("newsHovered")) {
+            img.src = News3
+        }
+    }
+    if (isHovered == false) {
+        if (number == 1) {
+            news1.classList.add("newsActive");
+            news2.classList.remove("newsActive");
+            news3.classList.remove("newsActive");
+            img.src = News1;
+        } else if (number == 2) {
+            news1.classList.remove("newsActive");
+            news2.classList.add("newsActive");
+            news3.classList.remove("newsActive");
+            img.src = News2;
+        } else if (number == 3) {
+            news1.classList.remove("newsActive");
+            news2.classList.remove("newsActive");
+            news3.classList.add("newsActive");
+            img.src = News3;}
+    }
+
+    console.log("terbaru" + isHovered);
+    }, [number])
 
     return (
         <div className={`containerNews-page ${tampilanMobile ? "mobile-containerNews-page" : "containerNews-page"}`}>
             <h2 className={`judulNews ${tampilanMobile ? "mobile-judulNews" : "judulNews"}`}>Popular News</h2>
             <section className={`containerNews ${tampilanMobile ? "mobile-containerNews" : "containerNews"}`}>
+                <img className={`imgNews ${
+                    tampilanMobile ? "mobile-imgNews" : "imgNews"
+                }`}></img>
                 <div className={`newsItem ${tampilanMobile ? "newsItem-mobile" : "newsItem"}`}>
                     <div className={`news1 ${tampilanMobile ? "news1-mobile" : "news1"}`}>
                         <h3>
